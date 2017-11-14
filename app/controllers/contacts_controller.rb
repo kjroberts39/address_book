28eb25d60965ce97677bdbe9b1bcc6ab46cb52ff
KJ Roberts <kjroberts39@hotmail.com>
2017-11-14 13:33:36 -0700

    added basic contact functionality, and added contacts index page as root or homepage

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,56 @@
+class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def edit
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to @contact, notice: 'Your new Contact was added.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @contact.update(contact_params)
+        format.html { redirect_to @contact, notice: 'Your Contact was successfully updated' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @contact.destroy
+    respond_to do |format|
+      format.html { redirect_to contacts_url, notice: 'Your contact was deleted.' }
+    end
+  end
+
+  private
+
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
+
+    def contact_params
+      params.require(:contact).permit(:name, :address, :email, :phone)
+    end
+end
